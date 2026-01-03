4be99754aca746e2992662f66b6b09b730ddc448
Rofiat Olabiyi <g00465381@atu.ie>
2026-01-03 19:06:49 +0000

    adding the foundation of the game

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -1,24 +1,105 @@
-﻿namespace CrossplatFinal
-{
-    public partial class MainPage : ContentPage
-    {
-        int count = 0;
+﻿namespace CrossplatFinal;
 
-        public MainPage()
+public partial class MainPage : ContentPage
+{
+    int currentLane = 1; // 0 = left, 1 = middle, 2 = right
+    int obstacleLane;
+    int score = 0;
+    bool isJumping = false;
+
+    IDispatcherTimer gameTimer;
+    Random random = new();
+
+    public MainPage()
+    {
+        InitializeComponent();
+        SetupGestures();
+        StartGame();
+    }
+
+    void SetupGestures()
+    {
+        var left = new SwipeGestureRecognizer { Direction = SwipeDirection.Left };
+        left.Swiped += (_, _) => MoveLeft();
+        
+
+        var right = new SwipeGestureRecognizer { Direction = SwipeDirection.Right };
+        right.Swiped += (_, _) => MoveRight();
+
+        var up = new SwipeGestureRecognizer { Direction = SwipeDirection.Up };
+        up.Swiped += (_, _) => Jump();
+
+        GameArea.GestureRecognizers.Add(left);
+        GameArea.GestureRecognizers.Add(right);
+        GameArea.GestureRecognizers.Add(up);
+    }
+
+    void StartGame()
+    {
+        obstacleLane = random.Next(0, 3);
+        Grid.SetColumn(Obstacle, obstacleLane);
+        Obstacle.TranslationY = -60;
+
+        gameTimer = Dispatcher.CreateTimer();
+        gameTimer.Interval = TimeSpan.FromMilliseconds(16); // ~60 FPS
+        gameTimer.Tick += GameLoop;
+        gameTimer.Start();
+    }
+
+    void GameLoop(object sender, EventArgs e)
+    {
+        Obstacle.TranslationY += 8;
+
+        if (Obstacle.TranslationY > Height)
         {
-            InitializeComponent();
+            Obstacle.TranslationY = -60;
+            obstacleLane = random.Next(0, 3);
+            Grid.SetColumn(Obstacle, obstacleLane);
+
+            score++;
+            ScoreLabel.Text = $"Score: {score}";
         }
 
-        private void OnCounterClicked(object? sender, EventArgs e)
+        CheckCollision();
+    }
+
+    void MoveLeft()
+    {
+        if (currentLane > 0)
         {
-            count++;
+            currentLane--;
+            Grid.SetColumn(Player, currentLane);
+        }
+    }
 
-            if (count == 1)
-                CounterBtn.Text = $"Clicked {count} time";
-            else
-                CounterBtn.Text = $"Clicked {count} times";
+    void MoveRight()
+    {
+        if (currentLane < 2)
+        {
+            currentLane++;
+            Grid.SetColumn(Player, currentLane);
+        }
+    }
 
-            SemanticScreenReader.Announce(CounterBtn.Text);
+    async void Jump()
+    {
+        if (isJumping) return;
+
+        isJumping = true;
+        await Player.TranslateTo(0, -120, 250);
+        await Player.TranslateTo(0, 0, 250);
+        isJumping = false;
+    }
+
+    void CheckCollision()
+    {
+        if (currentLane != obstacleLane)
+            return;
+
+        if (Obstacle.TranslationY > Height - 150 && !isJumping)
+        {
+            gameTimer.Stop();
+            DisplayAlert("Game Over", $"Score: {score}", "OK");
         }
     }
 }
