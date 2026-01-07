a9d46130d9c632a302cba2c20e958883257dc70b
Rofiat Olabiyi <g00465381@atu.ie>
2026-01-07 14:32:22 +0000

    adding settings c# code sound effects, music and difficulty

--- SettingsPage.xaml.cs
+++ SettingsPage.xaml.cs
@@ -1,1 +1,27 @@
-﻿
+﻿namespace CrossplatFinal;
+
+public partial class SettingsPage : ContentPage
+{
+    public SettingsPage()
+    {
+        InitializeComponent();
+
+        // Load saved values (defaults)
+        SoundSwitch.IsToggled = Preferences.Get("sound", true);
+        MusicSwitch.IsToggled = Preferences.Get("music", true);
+        DifficultyPicker.SelectedIndex = Preferences.Get("difficulty", 1); // 0 easy, 1 normal, 2 hard
+    }
+
+    private void Save_Clicked(object sender, EventArgs e)
+    {
+        Preferences.Set("sound", SoundSwitch.IsToggled);
+        Preferences.Set("music", MusicSwitch.IsToggled);
+        Preferences.Set("difficulty", DifficultyPicker.SelectedIndex);
+    }
+
+    private async void Back_Clicked(object sender, EventArgs e)
+    {
+        await Navigation.PopAsync();
+    }
+}
+
