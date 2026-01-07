ccf112e23eab11934b5d18ce4153cddba88d7c21
Rofiat Olabiyi <g00465381@atu.ie>
2026-01-07 18:02:20 +0000

    beginning pickups file

--- Pickups.cs
+++ Pickups.cs
@@ -1,4 +1,18 @@
-﻿
+﻿using Microsoft.Maui.Controls;
 
+namespace CrossplatFinal;
 
+public class Pickups
+{
+    //declaring variabeles
+    private readonly Image pickup;
+    private readonly Random random;
 
+    private double speed = 5;
+
+    public Pickups(Image pickupImage)
+    {
+        pickup = pickupImage;
+        random = new Random();
+    }
+}
